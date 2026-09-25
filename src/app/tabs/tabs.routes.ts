97ee439e4c1b97461679bd4bd9492cadc04ae763
opiserblengblong <qltizon@tip.edu.ph>
2026-09-26 01:07:38 +0800

    Update tabs.routes.ts

--- src/app/tabs/tabs.routes.ts
+++ src/app/tabs/tabs.routes.ts
@@ -1,15 +1,16 @@
 import { Routes } from '@angular/router';
 import { TabsPage } from './tabs.page';
+import { customerGuard } from '../admin/guards/customer.guard';
 
 export const TABS_ROUTES: Routes = [
   {
     path: '',
     component: TabsPage,
     children: [
-      { path: 'home', loadComponent: () => import('../pages/home/home.page').then((m) => m.HomePage) },
-      { path: 'shop', loadComponent: () => import('../pages/shop/shop.page').then((m) => m.ShopPage) },
-      { path: 'wishlist', loadComponent: () => import('../pages/wishlist/wishlist.page').then((m) => m.WishlistPage) },
-      { path: 'cart', loadComponent: () => import('../pages/cart/cart.page').then((m) => m.CartPage) },
+      { path: 'home', canActivate: [customerGuard], loadComponent: () => import('../pages/home/home.page').then((m) => m.HomePage) },
+      { path: 'shop', canActivate: [customerGuard], loadComponent: () => import('../pages/shop/shop.page').then((m) => m.ShopPage) },
+      { path: 'wishlist', canActivate: [customerGuard], loadComponent: () => import('../pages/wishlist/wishlist.page').then((m) => m.WishlistPage) },
+      { path: 'cart', canActivate: [customerGuard], loadComponent: () => import('../pages/cart/cart.page').then((m) => m.CartPage) },
       { path: 'account', loadComponent: () => import('../pages/account/account.page').then((m) => m.AccountPage) },
       { path: '', redirectTo: 'home', pathMatch: 'full' },
     ],
