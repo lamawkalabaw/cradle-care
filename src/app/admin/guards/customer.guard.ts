98d4953477b53e554e3c2495c09a0dcff6601444
opiserblengblong <qltizon@tip.edu.ph>
2026-09-26 01:26:18 +0800

    Update customer.guard.ts

--- src/app/admin/guards/customer.guard.ts
+++ src/app/admin/guards/customer.guard.ts
@@ -2,9 +2,11 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
-export const customerGuard: CanActivateFn = () => {
+export const customerGuard: CanActivateFn = async () => {
   const authService = inject(AuthService);
   const router = inject(Router);
+
+  await authService.waitForReady();
 
   if (authService.isLoggedIn()) {
     return true;
